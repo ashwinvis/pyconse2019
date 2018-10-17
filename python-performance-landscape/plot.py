9e8aae3f9f0f7c8d6dcf7a93957665cdd39bce85
Ashwin Vishnu <avmo@kth.se>
2018-10-17 10:29:20 +0200

    Black

--- python-performance-landscape/plot.py
+++ python-performance-landscape/plot.py
@@ -3,15 +3,36 @@ import networkx as nx
 from networkx.readwrite.adjlist import read_adjlist
 
 colors = {
-    'royalblue': ('Python', 'Cython', 'Pythran', 'Numba', 'numpy.f2py', 'PyPy',
-             'pyjulia', 'mpi4py', 'multiprocessing', 'threading', 'asyncio',
-             'numba', 'dask', 'trio',),
-    'pink': ('C', 'SWIG', 'C++', 'Boost.Python', 'xproject', 'xsimd',
-             'xtensor', 'xtensor-python',),
-    'goldenrod': ('Rust', 'rust-numpy',),
-    'grey': ('Fortran',),
-    'forestgreen': ('Julia', 'PyCall.jl',),
-    'slategrey': ('CUDA', 'OpenMP', 'MPI', 'SIMD',),
+    "royalblue": (
+        "Python",
+        "Cython",
+        "Pythran",
+        "Numba",
+        "numpy.f2py",
+        "PyPy",
+        "pyjulia",
+        "mpi4py",
+        "multiprocessing",
+        "threading",
+        "asyncio",
+        "numba",
+        "dask",
+        "trio",
+    ),
+    "pink": (
+        "C",
+        "SWIG",
+        "C++",
+        "Boost.Python",
+        "xproject",
+        "xsimd",
+        "xtensor",
+        "xtensor-python",
+    ),
+    "goldenrod": ("Rust", "rust-numpy"),
+    "grey": ("Fortran",),
+    "forestgreen": ("Julia", "PyCall.jl"),
+    "slategrey": ("CUDA", "OpenMP", "MPI", "SIMD"),
 }
 
 G = read_adjlist("graph.txt")
@@ -20,5 +41,5 @@ for node in G.nodes():
     color = [k for k, v in colors.items() if node in v]
     node_color.append(color[0])
 
-nx.draw(G, with_labels=True, node_shape='s', node_color=node_color)
+nx.draw(G, with_labels=True, node_shape="s", node_color=node_color)
 plt.show()
