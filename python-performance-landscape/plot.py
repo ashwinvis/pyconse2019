cf81bc05d45e2bc4c76362dddc73f421e3480f00
Ashwin Vishnu <avmo@kth.se>
2019-04-17 10:47:27 +0200

    Update with upcoming GPGPU accelerators

--- python-performance-landscape/plot.py
+++ python-performance-landscape/plot.py
@@ -8,6 +8,9 @@ colors = {
         "Cython",
         "Pythran",
         "Numba",
+        "Cupy",
+        "Cocos",
+        "hpat",
         "numpy.f2py",
         "PyPy",
         "pyjulia",
@@ -28,17 +31,21 @@ colors = {
         "xsimd",
         "xtensor",
         "xtensor-python",
+        "Bohrium",
+        "ArrayFire"
     ),
     "goldenrod": ("Rust", "rust-numpy"),
     "grey": ("Fortran",),
     "forestgreen": ("Julia", "PyCall.jl"),
-    "slategrey": ("CUDA", "OpenMP", "MPI", "SIMD"),
+    "slategrey": ("CUDA", "OpenCL", "OpenMP", "MPI", "SIMD"),
 }
 
 G = read_adjlist("graph.txt")
 node_color = []
 for node in G.nodes():
     color = [k for k, v in colors.items() if node in v]
+    if not color:
+        raise ValueError(f"Color undefined for {node}")
     node_color.append(color[0])
 
 nx.draw(G, with_labels=True, node_shape="s", node_color=node_color)
